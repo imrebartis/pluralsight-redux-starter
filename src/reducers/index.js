99bcc875aae51ef9821a642a96fc365d28730c68
Imre Bartis <imre.bartis@siili.com>
2017-11-17 12:18:51 +0200

    author reducer & initial state created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
 
 const rootReducer = combineReducers({
     //shorthand property name
     //(see e.g. https://ariya.io/2013/02/es6-and-object-literal-property-value-shorthand):
-    courses 
+    courses,
+    authors
 });
 
 export default rootReducer;
