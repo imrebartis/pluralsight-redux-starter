0e1bd74c9361ddfc73a875bfca4102975b0bd1d2
Imre Bartis <imre.bartis@siili.com>
2017-11-17 09:14:39 +0200

    inline form removed

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -6,26 +6,6 @@ import * as courseActions from '../../actions/courseActions';
 class CoursesPage extends Component {
     constructor(props, context) {
         super(props, context);
-        
-        this.state = {
-            course: { title: "" }  
-        };
-        
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-    
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course});
-    }
-    
-    onClickSave() {
-        // alert(`Saving ${this.state.course.title}`); 
-        // this.props.dispatch(courseActions.createCourse(this.state.course));
-        // this.props.createCourse(this.state.course);
-        this.props.actions.createCourse(this.state.course);      
     }
 
     courseRow(course, index) {
@@ -37,15 +17,6 @@ class CoursesPage extends Component {
             <div>
                 <h1>Courses</h1>
                 {this.props.courses.map(this.courseRow)}     
-                <h2>Add Course</h2>
-                <input 
-                    type="text" 
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title} />
-                <input 
-                    type="submit"
-                    value="Save"
-                    onClick={this.onClickSave} />
             </div>
         );
     }
