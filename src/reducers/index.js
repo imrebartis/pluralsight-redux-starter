8392768bbcfa1842969208bb8a54ef8f6c1cef65
Imre Bartis <imre.bartis@siili.com>
2017-11-20 16:58:37 +0200

    ajaxStatusReducer created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
     //shorthand property name
     //(see e.g. https://ariya.io/2013/02/es6-and-object-literal-property-value-shorthand):
     courses,
-    authors
+    authors,
+    ajaxCallsInProgress
 });
 
 export default rootReducer;
